22165a6bb30dd6250caf63f1cfeab2743d31c78a
Jason Kang <jason.kang@idexx.com>
2026-04-01 16:21:45 +1300

    brew compatible zsh-autocomplete

--- plugins/zsh-autocomplete.sh
+++ plugins/zsh-autocomplete.sh
@@ -1,5 +1,16 @@
 #! /usr/bin/bash
 
 # https://github.com/marlonrichert/zsh-autocomplete
-# sudo pacman -Syu zsh-autocomplete
-source /usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+# Arch: sudo pacman -Syu zsh-autocomplete
+# macOS: brew install zsh-autocomplete
+
+_zsh_autocomplete_plugin=
+if command -v brew >/dev/null 2>&1; then
+  _zsh_autocomplete_plugin="$(brew --prefix zsh-autocomplete 2>/dev/null)/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
+  [[ -f "$_zsh_autocomplete_plugin" ]] || _zsh_autocomplete_plugin=
+fi
+if [[ -z "$_zsh_autocomplete_plugin" ]]; then
+  _zsh_autocomplete_plugin=/usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+fi
+source "$_zsh_autocomplete_plugin"
+unset _zsh_autocomplete_plugin
